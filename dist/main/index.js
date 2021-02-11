e8eed38c79debe82e34d7a9aba93fca14f0db63b
Joshua Ma <me@joshma.com>
2021-02-10 22:27:18 -0800

    Add node-typescript builder
    This builder takes in an entrypoint and finds a working directory based
    on the nearest directory with package.json.
    
    From there, if package-lock.json is found, it'll use npm install. Or
    else, it'll use yarn.
    
    It'll use typescript's tsc to compile to a build directory, and then
    sets a Docker entrypoint running the entrypoint script out of the build
    directory.

--- dist/main/index.js
+++ dist/main/index.js
@@ -15089,6 +15089,8 @@ var buildpack_awaiter = (undefined && undefined.__awaiter) || function (thisArg,
     });
 };
 
+
+
 function getDockerfile(b) {
     return buildpack_awaiter(this, void 0, void 0, function* () {
         let contents = "";
@@ -15123,6 +15125,58 @@ function getDockerfile(b) {
 
       USER deno
       ENTRYPOINT ["deno", "run", "-A", "${b.builderConfig.entrypoint}"]
+    `;
+        }
+        else if (b.builder === "node-typescript") {
+            // Builder runs node Docker image, installs using npm (if package-lock.json) else yarn, then compiles using tsc
+            const entrypoint = b.builderConfig.entrypoint;
+            const entryDir = external_path_.dirname(entrypoint);
+            const pathParts = entryDir.split(external_path_.sep);
+            // Find the closest directory to entrypoint as working directory
+            let workingDir = null;
+            while (pathParts.length >= 0) {
+                if ((0,external_fs_.existsSync)(external_path_.join(...pathParts, "package.json"))) {
+                    workingDir = external_path_.join(...pathParts);
+                    break;
+                }
+                pathParts.pop();
+            }
+            if (workingDir === null) {
+                throw new Error(`Could not find package.json in any directories above ${b.builderConfig.entrypoint}`);
+            }
+            let installCommand;
+            const installFiles = [external_path_.join(workingDir, "package.json")];
+            if ((0,external_fs_.existsSync)(external_path_.join(workingDir, "package-lock.json"))) {
+                installCommand = "npm install";
+                installFiles.push(external_path_.join(workingDir, "package-lock.json"));
+                core.info(`Detected package-lock.json, running: ${installCommand}`);
+            }
+            else {
+                installCommand = "yarn";
+                if ((0,external_fs_.existsSync)(external_path_.join(workingDir, "yarn.lock"))) {
+                    installFiles.push(external_path_.join(workingDir, "yarn.lock"));
+                }
+                core.info(`Using default install command: ${installCommand}`);
+            }
+            const buildDir = ".airplane-build";
+            const relativeEntrypointJS = external_path_.relative(workingDir, b.builderConfig.entrypoint)
+                .replace(/\.ts$/, ".js");
+            contents = `
+      FROM node:15.8-stretch
+
+      RUN npm install -g typescript@4.1
+      WORKDIR /airplane
+      
+      COPY ${installFiles.join(" ")} ./
+      RUN ${installCommand}
+      
+      COPY ${workingDir} ./
+      RUN echo "Cleaning ${buildDir} in case it exists" \
+          && rm -rf ${buildDir}/ \
+          && echo "Running tsc" \
+          && tsc --outDir ${buildDir}/
+      
+      ENTRYPOINT ["node", "${buildDir}/${relativeEntrypointJS}"]
     `;
         }
         else if (b.builder === "docker") {
--- src/buildpack.ts
+++ src/buildpack.ts
@@ -1,24 +1,32 @@
-import { promises as fs } from "fs";
+import * as core from "@actions/core";
+import { existsSync, promises as fs } from "fs";
+import * as path from "path";
 
 export type Builder =
   | {
-    builder: "go";
-    builderConfig: {
-      entrypoint: string;
-    };
-  }
+      builder: "go";
+      builderConfig: {
+        entrypoint: string;
+      };
+    }
   | {
-    builder: "deno";
-    builderConfig: {
-      entrypoint: string;
-    };
-  }
+      builder: "deno";
+      builderConfig: {
+        entrypoint: string;
+      };
+    }
   | {
-    builder: "docker";
-    builderConfig: {
-      dockerfile: string;
+      builder: "node-typescript";
+      builderConfig: {
+        entrypoint: string;
+      };
+    }
+  | {
+      builder: "docker";
+      builderConfig: {
+        dockerfile: string;
+      };
     };
-  };
 
 export async function getDockerfile(b: Builder): Promise<string> {
   let contents = "";
@@ -52,6 +60,59 @@ export async function getDockerfile(b: Builder): Promise<string> {
 
       USER deno
       ENTRYPOINT ["deno", "run", "-A", "${b.builderConfig.entrypoint}"]
+    `;
+  } else if (b.builder === "node-typescript") {
+    // Builder runs node Docker image, installs using npm (if package-lock.json) else yarn, then compiles using tsc
+    const entrypoint = b.builderConfig.entrypoint;
+    const entryDir = path.dirname(entrypoint);
+    const pathParts = entryDir.split(path.sep);
+    // Find the closest directory to entrypoint as working directory
+    let workingDir = null;
+    while (pathParts.length >= 0) {
+      if (existsSync(path.join(...pathParts, "package.json"))) {
+        workingDir = path.join(...pathParts);
+        break;
+      }
+      pathParts.pop();
+    }
+    if (workingDir === null) {
+      throw new Error(
+        `Could not find package.json in any directories above ${b.builderConfig.entrypoint}`
+      );
+    }
+    let installCommand;
+    const installFiles = [path.join(workingDir, "package.json")];
+    if (existsSync(path.join(workingDir, "package-lock.json"))) {
+      installCommand = "npm install";
+      installFiles.push(path.join(workingDir, "package-lock.json"));
+      core.info(`Detected package-lock.json, running: ${installCommand}`);
+    } else {
+      installCommand = "yarn";
+      if (existsSync(path.join(workingDir, "yarn.lock"))) {
+        installFiles.push(path.join(workingDir, "yarn.lock"));
+      }
+      core.info(`Using default install command: ${installCommand}`);
+    }
+    const buildDir = ".airplane-build";
+    const relativeEntrypointJS = path
+      .relative(workingDir, b.builderConfig.entrypoint)
+      .replace(/\.ts$/, ".js");
+    contents = `
+      FROM node:15.8-stretch
+
+      RUN npm install -g typescript@4.1
+      WORKDIR /airplane
+      
+      COPY ${installFiles.join(" ")} ./
+      RUN ${installCommand}
+      
+      COPY ${workingDir} ./
+      RUN echo "Cleaning ${buildDir} in case it exists" \
+          && rm -rf ${buildDir}/ \
+          && echo "Running tsc" \
+          && tsc --outDir ${buildDir}/
+      
+      ENTRYPOINT ["node", "${buildDir}/${relativeEntrypointJS}"]
     `;
   } else if (b.builder === "docker") {
     return await fs.readFile(b.builderConfig.dockerfile, {
