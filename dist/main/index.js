53284a068ca15c5b4a554def95d1d0f82c913cee
Joshua Ma <me@joshma.com>
2021-02-11 22:14:40 -0800

    Rebuild

--- dist/main/index.js
+++ dist/main/index.js
@@ -15165,12 +15165,7 @@ function getDockerfile(b) {
           RUN ${installCommand}
           
           COPY ${projectRoot} ./
-          RUN [ -f tsconfig.json ] || cat >tsconfig.json <<TSCONFIG
-          {
-            "include": ["*", "**/*"],
-            "exclude": ["node_modules"]
-          }
-          TSCONFIG
+          RUN [ -f tsconfig.json ] || echo '{"include": ["*", "**/*"], "exclude": ["node_modules"]}' >tsconfig.json
           RUN rm -rf ${buildDir}/ && tsc --outDir ${buildDir}/ --rootDir .
           
           ENTRYPOINT ["node", "${buildDir}/${entrypointJS}"]
