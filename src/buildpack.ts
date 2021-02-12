cecd801be347eaca1084554a0af72956812f1dc0
Joshua Ma <me@joshma.com>
2021-02-11 22:05:20 -0800

    Remove extra echo

--- src/buildpack.ts
+++ src/buildpack.ts
@@ -112,7 +112,7 @@ export async function getDockerfile(b: Builder): Promise<string> {
           RUN ${installCommand}
           
           COPY ${projectRoot} ./
-          RUN [ -f tsconfig.json ] || echo echo '{"include": ["*", "**/*"], "exclude": ["node_modules"]}' >tsconfig.json
+          RUN [ -f tsconfig.json ] || echo '{"include": ["*", "**/*"], "exclude": ["node_modules"]}' >tsconfig.json
           RUN rm -rf ${buildDir}/ && tsc --outDir ${buildDir}/ --rootDir .
           
           ENTRYPOINT ["node", "${buildDir}/${entrypointJS}"]
