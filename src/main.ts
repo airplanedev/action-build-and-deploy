67474f132fb91fe870e63f7dc8d400da54ca8cb0
Joshua Ma <josh@airplane.dev>
2021-03-18 17:57:19 -0700

    Replace usages of replaceAll with replace (#27)
    replaceAll is not supported in node12, which GHA uses

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,8 @@ import { exec } from "./exec";
 import { promises as fs } from "fs";
 import path from "path";
 import { tmpDir } from "./tmp";
-import { getDockerfile, Builder } from './buildpack';
-import hash from 'object-hash'
+import { getDockerfile, Builder } from "./buildpack";
+import hash from "object-hash";
 
 async function run(): Promise<void> {
   try {
@@ -17,7 +17,9 @@ async function run(): Promise<void> {
 }
 
 async function main() {
-  core.debug(`Triggered run for context=${JSON.stringify(github.context, null, 2)}`)
+  core.debug(
+    `Triggered run for context=${JSON.stringify(github.context, null, 2)}`
+  );
 
   const apiKey: string = core.getInput("api-key");
   // TODO: remove this dependency on the team id
@@ -25,8 +27,13 @@ async function main() {
   const host: string = core.getInput("host");
   const parallel = core.getInput("parallel") === "true";
   const rawBuildArgs = core.getInput("build-args");
-  const buildArgs = rawBuildArgs.split('\n').map(arg => arg.trim()).filter(arg => arg !== "")
-  core.debug(`got rawBuildArgs='${rawBuildArgs}' translated into buildArgs=${buildArgs}`)
+  const buildArgs = rawBuildArgs
+    .split("\n")
+    .map((arg) => arg.trim())
+    .filter((arg) => arg !== "");
+  core.debug(
+    `got rawBuildArgs='${rawBuildArgs}' translated into buildArgs=${buildArgs}`
+  );
   const tasks = await getTasks(host, apiKey, teamID);
 
   // Get an Airplane Registry token:
@@ -63,50 +70,50 @@ async function main() {
   // Create a temporary directory for building all images in.
   await tmpDir();
 
-  const tags = await getTags()
+  const tags = await getTags();
   // Group together tasks by build pack, so that we build the minimum number of images.
-  const builds: Record<string, { b: Builder, tasks: Task[], imageTags: string[] }> = {}
+  const builds: Record<
+    string,
+    { b: Builder; tasks: Task[]; imageTags: string[] }
+  > = {};
   for (const task of tasks) {
     const b = {
       builder: task.builder,
       builderConfig: task.builderConfig,
-    } as Builder
-    const key = hash(b)
+    } as Builder;
+    const key = hash(b);
     builds[key] = {
       b,
-      tasks : [
-        ...(builds[key]?.tasks || []),
-        task,
-      ],
+      tasks: [...(builds[key]?.tasks || []), task],
       imageTags: [
         ...(builds[key]?.imageTags || []),
         ...tags.map((tag) => `${resp.repo}/${toImageName(task.taskID)}:${tag}`),
       ],
-    }
+    };
   }
 
   // Build and publish each image:
   console.log(`Uploading ${tasks.length} task(s) to Airplane...`);
-  let results: PromiseSettledResult<typeof builds[0]>[] = []
+  let results: PromiseSettledResult<typeof builds[0]>[] = [];
   if (parallel) {
     results = await Promise.allSettled(
-      Object.values(builds).map(async build => {
+      Object.values(builds).map(async (build) => {
         try {
-          await buildTask(build.b, build.imageTags, buildArgs)
+          await buildTask(build.b, build.imageTags, buildArgs);
         } catch (err) {
-          throw { build, err }
+          throw { build, err };
         }
-        return build
+        return build;
       })
     );
   } else {
     for (const build of Object.values(builds)) {
       try {
-        await buildTask(build.b, build.imageTags, buildArgs)
+        await buildTask(build.b, build.imageTags, buildArgs);
         results.push({
           status: "fulfilled",
           value: build,
-        })
+        });
       } catch (err) {
         results.push({
           status: "rejected",
@@ -114,77 +121,100 @@ async function main() {
             build,
             err,
           },
-        })
+        });
       }
     }
   }
 
-  console.table(results.map(result => {
-    const build: typeof builds[0] = result.status === "fulfilled" ? result.value : result.reason.build
-    if (!build) {
-      console.error(`build is undefined? for result: ${JSON.stringify(result)}`)
-    }
-    return {
-      status: result.status === "fulfilled" ? "✅" : "❌",
-      builder: build.b.builder,
-      builderConfig: JSON.stringify(build.b.builderConfig),
-      error: result.status === "fulfilled" ? "" : result.reason.err,
-      tasks: build.tasks.map(task => `https://app.airplane.dev/tasks/${task.taskID}`).join(", "),
-      tags: build.imageTags.join(", "),
-    }
-  }))
+  console.table(
+    results.map((result) => {
+      const build: typeof builds[0] =
+        result.status === "fulfilled" ? result.value : result.reason.build;
+      if (!build) {
+        console.error(
+          `build is undefined? for result: ${JSON.stringify(result)}`
+        );
+      }
+      return {
+        status: result.status === "fulfilled" ? "✅" : "❌",
+        builder: build.b.builder,
+        builderConfig: JSON.stringify(build.b.builderConfig),
+        error: result.status === "fulfilled" ? "" : result.reason.err,
+        tasks: build.tasks
+          .map((task) => `https://app.airplane.dev/tasks/${task.taskID}`)
+          .join(", "),
+        tags: build.imageTags.join(", "),
+      };
+    })
+  );
 
-  let numFailed = 0
+  let numFailed = 0;
   for (let result of results) {
     if (result.status === "rejected") {
-      numFailed++
+      numFailed++;
     }
   }
   if (numFailed > 0) {
-    throw new Error(`${numFailed}/${Object.keys(builds).length} builds failed. Review the table and logs above for more information.`)
+    throw new Error(
+      `${numFailed}/${
+        Object.keys(builds).length
+      } builds failed. Review the table and logs above for more information.`
+    );
   }
 
-  console.log('Done. Ready to launch from https://app.airplane.dev 🛫');
-  console.log(`These tasks can be run with your latest code using any of the following image tags: [${tags}]`)
+  console.log("Done. Ready to launch from https://app.airplane.dev 🛫");
+  console.log(
+    `These tasks can be run with your latest code using any of the following image tags: [${tags}]`
+  );
 }
 
 async function getTags() {
   // Fetch the shortest unique SHA (of length at least 7):
   const { stdout: shortSHA } = await exec([
-    "git", "rev-parse", "--short=7", github.context.sha
-  ])
+    "git",
+    "rev-parse",
+    "--short=7",
+    github.context.sha,
+  ]);
 
-  const branch = github.context.ref.replace(/^refs\/heads\//, "")
+  const branch = github.context.ref.replace(/^refs\/heads\//, "");
   const sanitizedBranch = sanitizeDockerTag(branch);
 
-  const tags = [shortSHA, sanitizedBranch]
+  const tags = [shortSHA, sanitizedBranch];
 
-  const defaultBranch = github.context.payload.repository?.default_branch
+  const defaultBranch = github.context.payload.repository?.default_branch;
   // The default branch should always be available, it just isn't included as a TS type above.
   // However, as a safety, I'm including a backup of some standard default branches:
-  const defaultBranches = defaultBranch == null ? ["main", "master"] : [defaultBranch]
-  core.debug(`Publishing :latest if defaultBranch=${defaultBranch} (-> ${defaultBranches}) is branch=${branch}`)
+  const defaultBranches =
+    defaultBranch == null ? ["main", "master"] : [defaultBranch];
+  core.debug(
+    `Publishing :latest if defaultBranch=${defaultBranch} (-> ${defaultBranches}) is branch=${branch}`
+  );
   if (defaultBranches.includes(branch)) {
-    tags.push("latest")
+    tags.push("latest");
   }
 
   return tags;
 }
 
 type Task = Builder & {
-  taskID: string
-}
+  taskID: string;
+};
 
-async function getTasks(host: string, apiKey: string, teamID: string): Promise<Task[]> {
+async function getTasks(
+  host: string,
+  apiKey: string,
+  teamID: string
+): Promise<Task[]> {
   // For backwards compatibility, accept a hardcoded list of tasks, if provided.
-  const tasksInput = core.getInput("tasks")
+  const tasksInput = core.getInput("tasks");
 
   // Translate the old format for buildpacks into the corresponding builders.
   // Note, we don't support newer builders or builder config here. Folks
   // that want to use those will want to remove the `tasks` input. The Action
   // will fetch the config from the Airplane API instead.
   const tasks = JSON.parse(tasksInput) as Array<{
-    taskID: string
+    taskID: string;
     buildPack:
       | {
           environment: "go";
@@ -198,38 +228,40 @@ async function getTasks(host: string, apiKey: string, teamID: string): Promise<T
           environment: "docker";
           dockerfile: string;
         };
-  }>
+  }>;
 
   if (tasks.length > 0) {
-    return tasks.map((t): Task => {
-      if (t.buildPack.environment === "go") {
-        return {
-          taskID: t.taskID,
-          builder: t.buildPack.environment,
-          builderConfig: {
-            entrypoint: t.buildPack.entrypoint,
-          },
+    return tasks.map(
+      (t): Task => {
+        if (t.buildPack.environment === "go") {
+          return {
+            taskID: t.taskID,
+            builder: t.buildPack.environment,
+            builderConfig: {
+              entrypoint: t.buildPack.entrypoint,
+            },
+          };
+        } else if (t.buildPack.environment === "deno") {
+          return {
+            taskID: t.taskID,
+            builder: t.buildPack.environment,
+            builderConfig: {
+              entrypoint: t.buildPack.entrypoint,
+            },
+          };
+        } else if (t.buildPack.environment === "docker") {
+          return {
+            taskID: t.taskID,
+            builder: t.buildPack.environment,
+            builderConfig: {
+              dockerfile: t.buildPack.dockerfile,
+            },
+          };
+        } else {
+          throw new Error("Unknown environment for taskID=" + t.taskID);
         }
-      } else if (t.buildPack.environment === "deno") {
-        return {
-          taskID: t.taskID,
-          builder: t.buildPack.environment,
-          builderConfig: {
-            entrypoint: t.buildPack.entrypoint,
-          },
-        }
-      } else if (t.buildPack.environment === "docker") {
-        return {
-          taskID: t.taskID,
-          builder: t.buildPack.environment,
-          builderConfig: {
-            dockerfile: t.buildPack.dockerfile,
-          },
-        }
-      } else {
-        throw new Error("Unknown environment for taskID=" + t.taskID)
       }
-    })
+    );
   }
 
   // Otherwise, fetch the task list from the API.
@@ -240,13 +272,14 @@ async function getTasks(host: string, apiKey: string, teamID: string): Promise<T
         "X-Team-ID": teamID,
       },
       searchParams: {
-        repo: `github.com/${github.context.repo.owner}/${github.context.repo.repo}`
-      }
-    }).json<{
-      tasks: Task[]
+        repo: `github.com/${github.context.repo.owner}/${github.context.repo.repo}`,
+      },
+    })
+    .json<{
+      tasks: Task[];
     }>();
-  
-  return resp.tasks
+
+  return resp.tasks;
 }
 
 async function buildTask(
@@ -254,16 +287,18 @@ async function buildTask(
   imageTags: string[],
   buildArgs: string[]
 ): Promise<void> {
-  core.debug(`${JSON.stringify({b, imageTags}, null, 2)}`)
+  core.debug(`${JSON.stringify({ b, imageTags }, null, 2)}`);
 
   // Generate a Dockerfile based on the build-pack:
-  const dir = await tmpDir(hash(b))
+  const dir = await tmpDir(hash(b));
   const dockerfilePath = path.join(dir, "Dockerfile");
   const dockerfile = await getDockerfile(b);
   await fs.writeFile(dockerfilePath, dockerfile);
-  core.debug(`wrote Dockerfile to ${dockerfilePath} with contents: \n${dockerfile}`);
+  core.debug(
+    `wrote Dockerfile to ${dockerfilePath} with contents: \n${dockerfile}`
+  );
 
-  const cacheDir = `/tmp/.buildx-cache/${hash(b)}`
+  const cacheDir = `/tmp/.buildx-cache/${hash(b)}`;
   await fs.mkdir(cacheDir, {
     recursive: true,
   });
@@ -273,7 +308,7 @@ async function buildTask(
     "buildx",
     "build",
     ...imageTags.map((tag) => ["--tag", tag]).flat(1),
-    ...buildArgs.map(arg => ["--build-arg", arg]).flat(1),
+    ...buildArgs.map((arg) => ["--build-arg", arg]).flat(1),
     "--file",
     dockerfilePath,
     "--cache-from",
@@ -297,8 +332,8 @@ function sanitizeDockerTag(str: string) {
   // start with a period or a dash and may contain a maximum of 128 characters.
   return str
     .substr(0, 128)
-    .replaceAll(/[^a-zA-Z0-9_.-]/g, "-")
-    .replaceAll(/^[.-]/g, "_");
+    .replace(/[^a-zA-Z0-9_.-]/g, "-")
+    .replace(/^[.-]/g, "_");
 }
 
 run();
