176f8137a5e382cdff9e86ac820e81f2fb7fbb8f
Colin King <me@colinking.co>
2021-03-29 11:52:48 -0500

    Fix image name choice

--- src/main.ts
+++ src/main.ts
@@ -323,7 +323,13 @@ async function buildTask(
 }
 
 function toImageName(taskID: string) {
-  return `task-${taskID.toLowerCase()}`;
+  let s = `task-${taskID.toLowerCase()}`
+  const c = s[s.length-1]
+  if (c >= '0' && c <= '9') {
+    s = s.substr(0, s.length-1) + 'a'
+  }
+
+  return s
 }
 
 function sanitizeDockerTag(str: string) {
